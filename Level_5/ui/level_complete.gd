5e9ccb988e91202fcb185c3b61c4a89875aced18
Dawit <dawit9514@gmail.com>
2025-04-22 20:34:35 +0300

    Updated level5 and changed the texture of the whole game

---
+++ Level_5/ui/level_complete.gd
@@ -0,0 +1,39 @@
+extends CanvasLayer
+
+@onready var next_level_button = $Panel/Button
+@onready var retry_button = $Panel/Button3
+@onready var exit_button = $Panel/Button2
+func _ready() -> void:
+	# Connect button signals
+	next_level_button.pressed.connect(_on_next_level_pressed)
+	retry_button.pressed.connect(_on_retry_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
+	# Pause game to focus on UI
+	#get_tree().paused = true
+
+
+func _on_next_level_pressed() -> void:
+	get_tree().paused = false
+	# Remove HUD before transitioning
+	var hud = get_tree().get_first_node_in_group("hud")
+	if hud:
+		hud.queue_free()
+	queue_free()
+	set_physics_process(true)
+	get_tree().change_scene_to_file("res://level6/main_game/scenes/game.tscn") 
+	print("Next Level button pressed")
+
+func _on_retry_pressed() -> void:
+	get_tree().paused = false
+	queue_free()
+	get_tree().change_scene_to_file("res://Level_5/scenes/game.tscn") 
+	print("Retry button pressed")
+
+func _on_exit_pressed() -> void:
+	# Remove HUD before exiting
+	var hud = get_tree().get_first_node_in_group("hud")
+	if hud:
+		hud.queue_free()
+	queue_free()
+	get_tree().quit()
+	print("Exit button pressed")
