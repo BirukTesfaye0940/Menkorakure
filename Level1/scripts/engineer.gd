d82bfe7eb25c7497d34e74410d43a666560fbf0d
abmoh4219 <abdelahmohammed0919@gmail.com>
2025-04-24 00:16:14 +0300

    animated the movment of the engineer

--- Level1/scripts/engineer.gd
+++ Level1/scripts/engineer.gd
@@ -1,27 +1,51 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-@onready var sprite_2d: Sprite2D = $Sprite2D  # Changed type to Sprite2D
-var nearby_monk = null
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -500.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
+var score = 0
+@onready var label = $Label
+var nearby_monk= null
+func add_point():
+	score +=1
+	label.text = "You collected" + " " + str(score) + " " + "coins."
 
 func _physics_process(delta: float) -> void:
-	# Removed animation logic since Sprite2D doesn't support animations
+	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle jump.
+	if Input.is_action_just_pressed("new_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left","move_right")
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("new_idle")
+		else:
+			animated_sprite.play("new_run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, 12)
-
+		animated_sprite.play("new_jump")
+		
+	if direction:
+		velocity.x = direction*SPEED
+	#elif Input.is_action_pressed("ui_up"):
+		#velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = SPEED
+	else:
+		velocity.x = move_toward(velocity.x,0,SPEED)
 	if nearby_monk and Input.is_action_just_pressed("interact"):
 		nearby_monk.interact()
-
 	move_and_slide()
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft  # This still works with Sprite2D
