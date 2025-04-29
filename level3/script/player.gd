9064f653a4dd862a08fdd0d58be805af570c0586
Yonas Zebrie <yonaszebrie@Biruks-MacBook-Pro.local>
2025-04-29 10:42:05 +0300

    fixed some issues with the characterbody2D

--- level3/script/player.gd
+++ level3/script/player.gd
@@ -26,6 +26,14 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("default")
+		else:
+			animated_sprite.play("walk")
+	else:
+		animated_sprite.play("jump")
 
 	if direction:
 		velocity.x = direction * current_speed
